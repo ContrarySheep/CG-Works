354c06729089169421965daa5f82e3740515b754
ContrarySheep <contrarysheep@gmail.com>
2012-04-12 09:45:29 -0400

    trying new rvm needs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,8 @@ set :user, CAP_PROFILE['production']['user']
 set :deploy_to, CAP_PROFILE['production']['path']
 set :port, CAP_PROFILE['production']['port']
 set :rvm_bin_path, CAP_PROFILE['production']['rvm_bin_path']
-set :rvm_ruby_string, CAP_PROFILE['production']['rvm_ruby_path']
+set :rvm_type, :system
+#set :rvm_ruby_string, CAP_PROFILE['production']['rvm_ruby_string']
 set :branch, CAP_PROFILE['production']['branch']
 set :domain, CAP_PROFILE['production']['server']
 set :deploy_env, "production"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ set :user, CAP_PROFILE['staging']['user']
 set :deploy_to, CAP_PROFILE['staging']['path']
 set :port, CAP_PROFILE['staging']['port']
 set :rvm_bin_path, CAP_PROFILE['staging']['rvm_bin_path']
-set :rvm_ruby_string, CAP_PROFILE['staging']['rvm_ruby_path']
+set :rvm_ruby_string, CAP_PROFILE['staging']['rvm_ruby_string']
 set :branch, CAP_PROFILE['staging']['branch']
 set :domain, CAP_PROFILE['staging']['server']
 set :deploy_env, "staging"
